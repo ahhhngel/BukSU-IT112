f723b7cabb09a5115b534bfb0a583544a13c9816
Angel Vasallo <73806325+ahhhngel@users.noreply.github.com>
2020-11-20 14:07:27 +0800

    Add files via upload

---
+++ consonantvowel.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+ {
+   char ch;
+   printf("Enter a character\n");
+   scanf("%c", &ch);
+
+   if( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+       ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+   {
+       printf("%c is vowel\n", ch);
+   }
+   else
+   {
+       printf("%c is a consonat\n", ch);
+   }
+ }
