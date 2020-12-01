5400eba7e8f8cb0325b04355bb25e16035efb7a8
Angel Vasallo <73806325+ahhhngel@users.noreply.github.com>
2020-12-01 18:03:59 +0800

    Add files via upload

---
+++ skip3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+   for(i=1;i<=30;i++)
+   {
+        if((i%3)==0)
+        {
+            continue;
+        }
+        printf("%3d",i);
+    }
+    return 0;
+
+}
